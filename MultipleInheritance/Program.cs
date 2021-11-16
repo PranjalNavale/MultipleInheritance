f7e39643023140669a8b4e81cd82d91246aa408a
Pranjal N <pranjalnavale992@gmail.com>
2021-11-15 21:12:18 -0600

    Add project files.

---
+++ MultipleInheritance/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseInterfaceChildNormal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IArea ar = new Circle();
+            ar.calculatearea();
+            ar.calculateperimeter();
+            ICircleArea cr = new Circle();
+            cr.calculatearea();
+            Circle cri = new Circle();
+            cri.calculateperimeter();
+
+        }
+
+        public interface IArea
+        {
+            void calculatearea();
+
+            void calculateperimeter();
+
+        }
+
+        public interface ICircleArea
+        {
+            void calculatearea();
+
+        }
+
+        public class Circle : IArea, ICircleArea
+        {
+             void IArea.calculatearea()
+            {
+                Console.WriteLine("Area  method implemented");
+            }
+
+            void ICircleArea.calculatearea()
+            {
+                Console.WriteLine("CircleArea method implemented");
+            }
+           
+
+            public void calculateperimeter()
+            {
+                Console.WriteLine("child class perimeter");
+            }
+
+        }
+
+
+    }
+}
